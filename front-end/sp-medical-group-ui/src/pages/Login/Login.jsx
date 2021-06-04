9289b3dde707702a10fb7ad2a46f3d97369c45a5
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-06-04 14:22:26 -0300

    Login Page - HTML and CSS completed

--- front-end/sp-medical-group-ui/src/pages/Login/Login.jsx
+++ front-end/sp-medical-group-ui/src/pages/Login/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import doctors from '../../img/doctors1.svg';
+import logo from '../../img/icons/spmed-full-logo.svg';
+
+import './Login.css';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Oi</h1>
+    <div className="login__container">
+      <div className="login__left-content">
+        <img
+          className="login__doctor-img"
+          src={doctors}
+          alt="Imagem de dois médicos"
+        />
+      </div>
+
+      <div className="login__right-content">
+        <div className="login__img-background-top">
+          <div className="login__logo-container">
+            <img src={logo} alt="Logo SP Medical Group" />
+          </div>
+        </div>
+
+        <form className="login__form">
+          <h2 className="login__form-title">Login</h2>
+          <label className="login__form-label">E-mail</label>
+          <input className="login__form-input" type="text" />
+          <label className="login__form-label">Senha</label>
+          <input className="login__form-input" type="password" />
+          <button className="login__form-btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
