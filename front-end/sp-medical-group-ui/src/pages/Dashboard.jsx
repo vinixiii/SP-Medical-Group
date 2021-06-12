b830462b896d7894c55308157e2a5996f9b09ded
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-06-12 20:10:14 -0300

    Fetch data for Dashboard cards

--- front-end/sp-medical-group-ui/src/pages/Dashboard.jsx
+++ front-end/sp-medical-group-ui/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 //Libs
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { RiStethoscopeFill } from 'react-icons/ri';
 import { FaUserMd, FaUserPlus, FaHospital } from 'react-icons/fa';
 
@@ -10,14 +11,86 @@ import '../styles/pages/Dashboard.css';
 import Card from '../components/Card';
 
 const Dashboard = () => {
+  const [doctorsNumber, setDoctorsNumber] = useState(0);
+  const [patientsNumber, setPatientsNumber] = useState(0);
+  const [clinicsNumber, setClinicsNumber] = useState(0);
+  const [specialtiesNumber, setSpecialtiesNumber] = useState(0);
+
+  const getDoctorsNumber = () => {
+    axios('http://localhost:5000/api/medicos', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setDoctorsNumber(res.data.length);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getPatientsNumber = () => {
+    axios('http://localhost:5000/api/pacientes', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setPatientsNumber(res.data.length);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getClinicsNumber = () => {
+    axios('http://localhost:5000/api/clinicas', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setClinicsNumber(res.data.length);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getSpecialtiesNumber = () => {
+    axios('http://localhost:5000/api/especialidades', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setSpecialtiesNumber(res.data.length);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getDoctorsNumber();
+    getPatientsNumber();
+    getClinicsNumber();
+    getSpecialtiesNumber();
+  });
+
   return (
     <section className="dashboard__container">
       <h1>Dashboard</h1>
       <div className="dashboard__content">
-        <Card number="3" title="Médico" icon={<FaUserMd />} />
-        <Card number="3" title="Pacientes" icon={<FaUserPlus />} />
-        <Card number="3" title="Clínicas" icon={<FaHospital />} />
-        <Card number="3" title="Especialidades" icon={<RiStethoscopeFill />} />
+        <Card number={doctorsNumber} title="Médico" icon={<FaUserMd />} />
+        <Card number={patientsNumber} title="Pacientes" icon={<FaUserPlus />} />
+        <Card number={clinicsNumber} title="Clínicas" icon={<FaHospital />} />
+        <Card
+          number={specialtiesNumber}
+          title="Especialidades"
+          icon={<RiStethoscopeFill />}
+        />
       </div>
     </section>
   );
